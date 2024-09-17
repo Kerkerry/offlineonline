ae8a2b8fab0a27e57110f50fb4fe488e333785fc
Kerry Wangamati <kerrywangamati24@gmail.com>
2024-09-17 12:21:54 +0300

    created data states and api provider class to fetch the data

---
+++ lib/features/home/data/data_source/remote/home_api_provider.dart
@@ -0,0 +1,28 @@
+import 'package:dio/dio.dart';
+import 'package:offlineapp/config/constants/api_constants.dart';
+import 'package:offlineapp/core/helper/log_helper.dart';
+
+class HomeApiProvider {
+  final Dio dio;
+  HomeApiProvider._(this.dio);
+
+  // Call home page products data
+  dynamic callHomeProductsEndpoint() async {
+    // request url
+    final requestUrl = EnvironmentVariables.getProduct;
+
+    final response = await dio
+        .request(
+      requestUrl,
+      options: Options(method: "GET"),
+    )
+        .onError(
+      (DioException error, stackTrace) {
+        logger.e(error);
+        throw error;
+      },
+    );
+    // response after the call is successful
+    return response;
+  }
+}
